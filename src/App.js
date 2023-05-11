7e1f454ee45c07991346788d2d743cd057b608d1
ababhin <abhinavbijpuria@gmail.com>
2023-05-11 10:41:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> pipeline completed :-)
+          Edit <code>src/App.js</code> hello pipeline :-)
         </p>
         <a
           className="App-link"
